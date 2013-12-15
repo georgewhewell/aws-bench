b72fc41b421e4494b48a7c692f54874a32acb9d4
georgewhewell <georgerw@gmail.com>
2013-12-15 23:48:21 +0000

    add resources

---
+++ benchmark.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import subprocess
+import time
+
+# expects minerd in ~/
+def benchmark_miner():
+    command = ['~/minerd', '--benchmark']
+    process = subprocess.Popen(command,
+        stderr=subprocess.PIPE,
+        stdout=subprocess.PIPE)
+    time.sleep(30)
+    process.kill()
+    lines = process.stderr.readlines()
+    for line in lines[::-1]:
+        if 'Total:' in line:
+            total = line.split(' ')[-2]
+
+    print total
+
+if __name__ == '__main__':
+    benchmark_miner()
